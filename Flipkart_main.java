fd9d94bcf8533852675734fd1145f3223791ee09
Pranav Sood <99885598+pranav-sood-12@users.noreply.github.com>
2023-06-21 11:41:37 +0530

    Create Flipkart_main.java

---
+++ Flipkart_main.java
@@ -0,0 +1,15 @@
+package first_lab;
+
+
+public class first_code {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubj
+		WorkingWithChrome sc = new WorkingWithChrome();
+		sc.invokeBrowser();
+		sc.navigate();
+		sc.closeTest();
+		
+	}
+
+}
